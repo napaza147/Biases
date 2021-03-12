8931cfd6309f657c47fd04d16e8fdf30974aad2c
napaza147 <77561611+napaza147@users.noreply.github.com>
2021-03-12 02:43:24 -0500

    Update models.py

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ from otree.api import (
     models, widgets, BaseConstants, BaseSubsession, BaseGroup, BasePlayer,
     Currency as c, currency_range
 )
-
+s
 import random
 import itertools
 import json
